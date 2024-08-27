34823faf02b247f91364693f51deb37ab28c6a8b
Ember Doyle <doyle.ember@gmail.com>
2024-08-27 08:36:12 -0600

    Code simpliest answer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(numA,numB) {
+    let integersSummed = 0;
+    if (numB>numA){ //4>1 == true
+        for(i=numA;numB>=i;i++){
+            integersSummed += i;
+            
+        }
+    }
+    else if (numA>numB){ 
 
+    }
+    else if(numA==numB){
+
+    }
+    return integersSummed
 };
 
 // Do not edit below this line
