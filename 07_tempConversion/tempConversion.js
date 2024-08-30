f505820f7a56aae86bb05f530d2a1907b40f480d
Ember Doyle <doyle.ember@gmail.com>
2024-08-30 08:15:05 -0600

    Complete assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  convert = (degree - 32)/1.8;
+  let result = Math.round(convert*10)/10;
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  convert = (degree * 1.8) + 32;
+  let result = Math.round(convert*10)/10;
+  return result
 };
 
 // Do not edit below this line
