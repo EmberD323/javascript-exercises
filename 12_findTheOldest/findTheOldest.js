8c71b2a1827f61b3deeb89611d77efe1134b801a
Ember Doyle <doyle.ember@gmail.com>
2024-09-09 17:21:30 -0600

    Complete assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+//sort by age
+    let sortedArray =  people.sort(compare);
+    const currentYear = 2024;
+    function compare (a,b){
+        if (a.hasOwnProperty('yearOfDeath') == false) {
+            a.yearOfDeath = 2024;
+        }
+        else if(b.hasOwnProperty('yearOfDeath') == false) {
+            b.yearOfDeath = 2024;
+        }
+        
+        if ((a.yearOfDeath - a.yearOfBirth)<(b.yearOfDeath - b.yearOfBirth)) return -1;
+        if ((a.yearOfDeath - a.yearOfBirth)==(b.yearOfDeath - b.yearOfBirth)) return 0;
+        if ((a.yearOfDeath - a.yearOfBirth)>(b.yearOfDeath - b.yearOfBirth)) return 1;
+    }
+    // return the oldest person
+    return sortedArray[sortedArray.length-1];
+    
 
+     /*inventors.sort(compare);
+    function compare (a,b){
+      if ((a.passed - a.year)<(b.passed - b.year)) return -1;
+      if ((a.passed - a.year)==(b.passed - b.year)) return 0;
+      if ((a.passed - a.year)>(b.passed - b.year)) return 1;
+      
+    }
+    console.table(inventors);*/
+//get array of age
+//sort 
+//take last
+//or reduce only
+// let oldestPerson = people.reduce(age(),)
 };
 
 // Do not edit below this line
