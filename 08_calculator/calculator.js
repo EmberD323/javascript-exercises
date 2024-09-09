44f3c01b707b0a0d6e1223242229805bba12cbb2
Ember Doyle <doyle.ember@gmail.com>
2024-09-09 10:28:45 -0600

    Complete assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,49 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
+  
+  
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length ==0){
+    return 0
+  }
+  else{
+    let arrAdd = array.reduce((total,currentItem) => {
+      return total + currentItem;
+    });
+    return arrAdd;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if(array.length ==0){
+    return 0
+  }
+  else{
+    let arrAdd = array.reduce((total,currentItem) => {
+      return total * currentItem;
+    });
+    return arrAdd;
+  }
+  
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  let ans =1;  
+  if (n == 0)
+      return 1;
+    for (let i = 2; i <= n; i++) 
+      ans = ans * i; 
+      return ans; 
 	
 };
 
